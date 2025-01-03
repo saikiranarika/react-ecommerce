9cc9ed8641a4bfd7a25f7661a0dbe58f2dc1e526
Nitinsetty <settynitin003@gmail.com.com>
2025-01-03 10:27:25 +0530

    hiiiiii

--- src/stores/data/mobiles.jsx
+++ src/stores/data/mobiles.jsx
@@ -1,121 +1,121 @@
 export const mobileData =[{
     "id": "1",
     "product":"Mobile",
-    "image":"/assets/mobiles/1.jpg",
+    "image":"https://m.media-amazon.com/images/I/71+QToFrHXL._SX679_.jpg",
     "company": "Apple",
-    "model": "i phone 13 pro",
-    "price": "100000.00",
+    "model": "Redmi Note 14 5G",
+    "price": "19,999.00",
     "category": "Electronics",
-    "discription": "the latest i phone "
+    "discription": "Display: 6.67 FHD+ AMOLED (1080x2400) Ultra-narrow bezels Display with 120Hz Refresh rate; 2100nits peak brightness; Corning Gorilla Glass 5 Display Protection "
 },
 {
     "id": "2",
     "product":"Mobile",
-    "image":"/assets/mobiles/2.jpg",
-    "company": "Apple",
-    "model": "i phone 13 pro",
-    "price": "100000.00",
+    "image":"https://m.media-amazon.com/images/I/71r0Ysx+oVL._SL1500_.jpg",
+    "company": "Realme ",
+    "model": "realme NARZO N61",
+    "price": "8,499.00",
     "category": "Electronics",
-    "discription": "the latest i phone "
+    "discription": "realme NARZO N61 (Voyage Blue,6GB RAM+128GB Storage) 90Hz Eye Comfort Display | IP54 Dust & Water Resistance | 48-Month Fluency | Charger in The Box"
 },
 {
     "id": "3",
     "product":"Mobile",
-    "image":"/assets/mobiles/3.png",
-    "company": "Apple",
-    "model": "i phone 13 pro",
-    "price": "100000.00",
+    "image":"https://m.media-amazon.com/images/I/81T3olLXpUL._SX679_.jpg",
+    "company": "Samsung Galaxy",
+    "model": "Samsung Galaxy M05",
+    "price": "6,999.00",
     "category": "Electronics",
-    "discription": "the latest i phone "
+    "discription": "Samsung Galaxy M05 (Mint Green, 4GB RAM, 64 GB Storage) | 50MP Dual Camera | Bigger 6.7 HD+ Display | 5000mAh Battery | 25W Fast Charging | 2 Gen OS Upgrade & 4 Year Security Update | Without Charger"
 },
 {
     "id": "4",
     "product":"Mobile",
-    "image":"/assets/mobiles/4.jpg",
-    "company": "Apple",
-    "model": "i phone 13 pro",
-    "price": "100000.00",
+    "image":"https://m.media-amazon.com/images/I/61LB+d0vheL._SL1500_.jpg",
+    "company": "OnePlus",
+    "model": "OnePlus Nord CE 3 Lite 5G",
+    "price": "15,160.00",
     "category": "Electronics",
-    "discription": "the latest i phone "
+    "discription": "OnePlus Nord CE 3 Lite 5G (Chromatic Gray, 8GB RAM, 128GB Storage) "
 },
 {
     "id": "5",
     "product":"Mobile",
-    "image":"/assets/mobiles/5.png",
-    "company": "Apple",
-    "model": "i phone 14 pro",
-    "price": "100000.00",
+    "image":"https://m.media-amazon.com/images/I/619VJYWIbXL._SX679_.jpg",
+    "company": "iQOO",
+    "model": "iQOO Z9 Lite 5G ",
+    "price": "11,498.00",
     "category": "Electronics",
-    "discription": "the latest i phone "
+    "discription": "iQOO Z9 Lite 5G (Mocha Brown, 6GB RAM, 128GB Storage) | Dimensity 6300 5G | 50MP Sony AI Camera | Charger in The Box"
 },
 {
     "id": "6",
     "product":"Mobile",
-    "image":"/assets/mobiles/6.jpg",
-    "company": "Apple",
-    "model": "i phone 13 pro",
-    "price": "100000.00",
+    "image":"https://m.media-amazon.com/images/I/712tqhtIw1L._SY741_.jpg",
+    "company": "POCO",
+    "model": "POCO C61",
+    "price": "5,999.00",
     "category": "Electronics",
-    "discription": "the latest i phone "
+    "discription": "POCO C61 Ethereal Blue 4GB RAM 64GB ROM "
 },
 {
     "id": "7",
     "product":"Mobile",
-    "image":"/assets/mobiles/7.jpg",
-    "company": "IQOO",
-    "model": "i phone 13 pro",
-    "price": "100000.00",
+    "image":"https://m.media-amazon.com/images/I/51M9zgRPtKL._SX679_.jpg",
+    "company": "Lava ",
+    "model": "Lava O3 Pro",
+    "price": "6,999.00",
     "category": "Electronics",
-    "discription": "the latest i phone "
+    "discription": "Lava O3 Pro (Glossy White, 4 GB RAM, 128 GB Storage)| Bigger 6.56 HD+ Display | 50 MP AI Triple Rear Camera | 8 MP Front Camera| 5000 mAh Battery | 10W Charging | Charger in Box "
 },
 {
     "id": "8",
     "product":"Mobile",
-    "image":"/assets/mobiles/8.jpg",
-    "company": "Realme",
-    "model": "narzo 6o pro",
-    "price": "100000.00",
+    "image":"https://m.media-amazon.com/images/I/71MGSe0lcML._SX679_.jpg",
+    "company": "Redmi",
+    "model": "Redmi Note 13 5G ",
+    "price": "15,220.00",
     "category": "Electronics",
-    "discription": "the latest i phone "
+    "discription": "Redmi Note 13 5G (Chromatic Purple, 8GB RAM, 256GB Storage) "
 },
 {
     "id": "9",
     "product":"Mobile",
-    "image":"/assets/mobiles/9.jpg",
-    "company": "Realme",
-    "model": "Realme 11 pro",
-    "price": "100000.00",
+    "image":"https://m.media-amazon.com/images/I/71m+dNHzoGL._SX569_.jpg",
+    "company": "Motorola",
+    "model": "Motorola Edge 50 Fusion 5G ",
+    "price": "26,130.00",
     "category": "Electronics",
-    "discription": "the latest i phone "
+    "discription": "Motorola Edge 50 Fusion 5G (Hot Pink, 12GB RAM, 256GB Storage)"
 },
 {
     "id": "10",
     "product":"Mobile",
-    "image":"/assets/mobiles/10.jpg",
-    "company": "One plus",
-    "model": "1+",
-    "price": "100000.00",
+    "image":"https://m.media-amazon.com/images/I/61VxzkiD1EL._SX679_.jpg",
+    "company": "Vivo",
+    "model": "Vivo T3 Lite 5G ",
+    "price": "11,479.00",
     "category": "Electronics",
-    "discription": "the latest i phone "
+    "discription": "Vivo T3 Lite 5G Smartphone (Vibrant Green, 6GB Ram 128GB Storage)"
 },
 {
     "id": "11",
     "product":"Mobile",
-    "image":"/assets/mobiles/11.jpg",
-    "company": "IQOO",
-    "model": "Neo 7 pro",
-    "price": "100000.00",
+    "image":"https://m.media-amazon.com/images/I/91wPh6RMcdL._SX679_.jpg",
+    "company": "OPPO",
+    "model": "OPPO F27 Pro+ 5G ",
+    "price": "27,999.00",
     "category": "Electronics",
-    "discription": "the latest i phone "
+    "discription": "OPPO F27 Pro+ 5G (Midnight Navy, 8GB RAM, 256GB Storage) | 6.7 FHD+ AMOLED Toughest 3D Curved Display|64MP AI Featured Camera|IP69 | 67W SUPERVOOC| with No Cost EMI/Additional Exchange Offers "
 },
 {
     "id": "12",
     "product":"Mobile",
-    "image":"/assets/mobiles/12.jpg",
-    "company": "One plus",
-    "model": "1+",
-    "price": "330000.00",
+    "image":"https://m.media-amazon.com/images/I/61aY+pnXKLL._SX679_.jpg",
+    "company": "Infinix",
+    "model": "Infinix Smart",
+    "price": "6,298.00",
     "category": "Electronics",
-    "discription": "the latest i phone "
+    "discription": "Infinix Smart 8 HD (64 GB) 3GB RAM (Crystal Green)"
 }
 ]
